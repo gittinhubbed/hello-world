203d4a0b6d9c21f10d595005ecb8210cae6bf4ac
gittinhubbed <graberj@appstate.edu>
2020-08-17 14:15:52 -0400

    partially done, new dialogue, exploring an alternative

--- Hello.java
+++ Hello.java
@@ -8,6 +8,9 @@ public class Hello {
         System.out.println("Hello Dr. Fenwick.");
         System.out.println("How are you?");
         String response = keyboard.nextLine();
+        System.out.println("What time is it?");
+        String response = keyboard.nextLine();
+
     }
 }
 
