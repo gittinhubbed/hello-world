d6f88e4297500013ca1a5f50a4d1df59f029da8a
gittinhubbed <graberj@appstate.edu>
2020-08-17 14:19:33 -0400

    Added more dialogue

--- Hello.java
+++ Hello.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
 /*
     Hello World Program.
 */
 
 public class Hello {
     public static void main(String[] args) {
-        java.util.Scanner keyboard = new java.util.Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Hello Dr. Fenwick.");
         System.out.println("How are you?");
         String response = keyboard.nextLine();
         System.out.println("What time is it?");
-        String response = keyboard.nextLine();
+        String response2 = keyboard.nextLine();
 
     }
 }
