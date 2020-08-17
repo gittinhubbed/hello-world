900c9fc372ab9357c739f2ae7d90fa43b9649640
gittinhubbed <graberj@appstate.edu>
2020-08-17 14:08:59 -0400

    Added dialogue

--- Hello.java
+++ Hello.java
@@ -4,7 +4,10 @@
 
 public class Hello {
     public static void main(String[] args) {
+        java.util.Scanner keyboard = new java.util.Scanner(System.in);
         System.out.println("Hello Dr. Fenwick.");
+        System.out.println("How are you?");
+        String response = keyboard.nextLine();
     }
 }
 
